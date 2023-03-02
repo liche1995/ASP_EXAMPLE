082ca705211233eea577280f80b9854520bb9abc
LC,Chueh <liche0241@gmail.com>
2023-03-02 08:46:53 +0800

    create "test" template and create post model

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using static System.TimeZoneInfo;
+using static System.DateTime;
 
 namespace ASP_example.Controllers;
 
@@ -8,5 +10,14 @@ public class TestController: Controller
     public string Index(int id)
     {
         return $"Hello 你好! ASP.NET core!\n{id}";
+    }
+
+    public IActionResult Template()
+    {
+        TimeZoneInfo timezone = Local;
+        DateTime now_time = DateTime.Now;
+        ViewData["time"] = now_time + " " + Local.Id;
+        ViewData["text"] = "hahaha";
+        return View();
     }
 }
---
+++ Models/Post.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using static System.DateTime;
+
+namespace ASP_example.Models;
+
+public class Post
+{
+    public int id  {get; set;}
+    public required string title {get; set;} //declear not nullable
+    public int author {get; set;}
+    public DateTime crtate_time {get; set;}
+    public DateTime edit_time {get; set;}
+    public bool soft_delete {get; set;}
+}
